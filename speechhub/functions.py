4236dd7c18a5b4d7c8a6ba6ff912d60f940566b4
alvesjnr <alvesjunior.antonio@gmail.com>
2012-03-27 11:43:59 +0200

    adding slugfy and changing the blog files structure

--- speechhub/functions.py
+++ speechhub/functions.py
@@ -22,15 +22,13 @@ def create_blog_structure(blog_path,blog_config):
         └── static
             └── css
     """
-    
-    if not os.listdir(blog_path):
-        raise NotEmptyFolderError()
-
-    os.makedirs(os.path.join(blog_path,'posts'))
-    os.makedirs(os.path.join(blog_path,'static'))
-    os.makedirs(os.path.join(blog_path,'statc/css'))
-    os.makedirs(os.path.join(blog_path,'pages'))
-    os.makedirs(os.path.join(blog_path,'config'))
+    slugfied_blog_name = blog_config['slugfied_blog_name']
+    os.makedirs(os.path.join(blog_path,slugfied_blog_name))
+    os.makedirs(os.path.join(blog_path,os.path.join(slugfied_blog_name,'posts')))
+    os.makedirs(os.path.join(blog_path,os.path.join(slugfied_blog_name,'static')))
+    os.makedirs(os.path.join(blog_path,os.path.join(slugfied_blog_name,os.path.join('statc','css'))))
+    os.makedirs(os.path.join(blog_path,os.path.join(slugfied_blog_name,'pages')))
+    os.makedirs(os.path.join(blog_path,os.path.join(slugfied_blog_name,'config')))
 
     create_empty_index(blog_path,blog_config)
 
@@ -42,3 +40,18 @@ def create_empty_index(blog_path,blog_config):
     with open(os.path.join(blog_path,'index.html'),'w') as index_file:
         index_content = pystache.render(index_template,blog_config)
         index_file.write(index_content)
+
+
+def slugify(text, delim=u'-'):
+    """Generates an slightly worse ASCII-only slug.
+    Originally from:
+    http://flask.pocoo.org/snippets/5/
+    Generating Slugs
+    By Armin Ronacher filed in URLs
+    """
+    result = []
+    for word in _punct_re.split(text.lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        if word:
+            result.append(word)
+    return unicode(delim.join(result))
