feb7de62a8a0eab45ef0a1bf8853f57ae6cf8882
alvesjnr <alvesjunior.antonio@gmail.com>
2012-03-29 10:46:54 +0200

    creating setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='SpeechHub',
+      version='0.1',
+      description='A simple static blog engine',
+      author='Antonio Ribeiro',
+      author_email='alvesjunior.antonio@gmail.com',
+      url='alvesjnr.github.com',
+      packages=['speechhub'],
+      entry_points = {
+        'console_scripts': [
+            'speechhub = speechhub.speechhub:main',
+        ],
+    }
+     )
